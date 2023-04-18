6eb8f51fd5e864a3e803169b38a94b4da9d4d22b
Nora <nsansaturnino@birt.eus>
2023-04-18 17:34:43 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Ejemplo para GitHub
+ */
+
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author ulhi
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
